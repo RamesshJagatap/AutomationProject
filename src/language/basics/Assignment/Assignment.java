4701343898a94e59d70ab299c329db772c909d6e
Ramesh Jagtap <rjagtap18699@gmail.com>
2025-04-08 23:24:14 +0530

    Assignment 1- DataTypes

---
+++ src/language/basics/Assignment/Assignment.java
@@ -0,0 +1,78 @@
+package language.basics.Assignment;
+
+public class Assignment {
+
+	public static void main(String[] args) {
+//		1. Temperature of a city in degrees Celsius: 25.5
+		float temprature = 25.5f;
+		System.out.println(temprature);
+
+//		2. Whether a customer has placed an order: true or false
+		boolean order = true;
+		boolean order1 = false;
+		System.out.println(order1);
+
+//		3. Person's phone number: "123-456-7890"
+		String number = "123-456-7890";
+		System.out.println(number);
+
+//		4. Amount of money in a customer's bank account: 1000.50
+		float money = 1000.50f;
+		System.out.println(money);
+
+//		5. Person's email address: "john.doe@example.com"
+		String EmailId = "john.doe@example.com";
+		System.out.println(EmailId);
+
+		// 6. Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+
+		// 7. Person's marital status: true or false
+		boolean maritalStatus = true | false;
+		System.out.println(maritalStatus);
+
+//		8. Person's occupation: "Software Engineer"
+		String Occupation = "Software Engineer";
+		String color = "blue";
+		System.out.println(Occupation);
+
+//		9. Person's favourite colour: "Blue"
+		int year = 2023;
+		System.out.println(year);
+
+		// 11.Number of followers on a social media platform: 1,000,000
+		int followers = 1000000;
+		System.out.println(followers);
+
+//		12.Rating of a movie: 7.5
+		float rating = 7.5f;
+		System.out.println(rating);
+
+		// 14.Title of a book: "To Kill a Mockingbird"
+		String TitleBook = "To Kill a Mockingbird";
+		System.out.println(TitleBook);
+
+//		15.Number of employees in a company: 500
+		short employees = 500;
+		System.out.println(employees);
+
+//		17.Name of a country: "United States"
+		String country = "United States";
+		System.out.println(country);
+
+//		18.Person's eye color: "Brown"
+		String EyeColor = "Brown";
+		System.out.println(EyeColor);
+
+//		19.Person's birthplace: "New York City"
+		String BirthPlace = "New York City";
+		System.out.println(BirthPlace);
+
+//		20.Distance between two cities: 200.5
+		float distance = 200.5f;
+		System.out.println(distance);
+		
+		/****** Completed ******/
+
+	}
+
+}
